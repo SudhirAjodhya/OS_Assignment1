e3c7f0834a9b6cdb8a0f7ff332bed8ff53716b19
SudhirAjodhya <sudhir.ajodhya@gmail.com>
2025-09-15 21:16:47 +0200

    implementing basic features

--- Wits-Shell-Tester/witsshell.c
+++ Wits-Shell-Tester/witsshell.c
@@ -7,9 +7,44 @@
 #include <assert.h>
 #include <fcntl.h>
 
+void interactive_mode();
+void batch_mode();
+void print_error();
+
 
 int main(int MainArgc, char *MainArgv[]){
-
+	
+	if (MainArgc == 1){
+		// Running in interactive mode
+		interactive_mode();
+	}
+	else if (MainArgc == 2){
+		// Running in batch mode
+		batch_mode();
+	}
+	else{
+		// Too many args throw an error
+		print_error();
+		return 1;
+	}
 
 	return(0);
+}
+
+void interactive_mode(){
+	_Bool running = 1;
+
+	printf("witsshell>\n");
+	while (running){
+		
+	}
+}
+
+void batch_mode(){
+
+}
+
+void print_error(){
+	char error_message[30] = "An error has occurred\n";
+	write(STDERR_FILENO, error_message, strlen(error_message));
 }
