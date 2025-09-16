cfad21e46e0d3cc99552b14f08b9e06ba2efb171
SudhirAjodhya <sudhir.ajodhya@gmail.com>
2025-09-16 18:19:46 +0200

    function to remove whitespace and new line

--- Wits-Shell-Tester/witsshell.c
+++ Wits-Shell-Tester/witsshell.c
@@ -8,9 +8,10 @@
 #include <fcntl.h>
 
 void interactive_mode();
-void batch_mode();
+void batch_mode(char* file_name);
+void handle_line_input(char* input);
 void print_error();
-
+void clean_user_input(char* input);
 
 int main(int MainArgc, char *MainArgv[]){
 	
@@ -20,7 +21,7 @@ int main(int MainArgc, char *MainArgv[]){
 	}
 	else if (MainArgc == 2){
 		// Running in batch mode
-		batch_mode();
+		batch_mode(MainArgv[1]);
 	}
 	else{
 		// Too many args throw an error
@@ -37,22 +38,24 @@ void interactive_mode(){
 	size_t len = 0;
 
 	while (running){
-		printf("witsshell>");
+		printf("witsshell> ");
 		fflush(stdout);
 		
-		if (getline(&line, &len, stdin) != -1){
-			//printf("The input from the user is: %swith length: %ld\n", line, len);
+		if (getline(&line, &len, stdin) == -1){
+			free(line);
+			exit(0);
 
-			// Remove trailing whitespace
-			line[strcspn(line, "\n")] = 0;
-		} 
-		else{ 
-			exit(0); 
 		}
+		else{
+			handle_line_input(line);
+		} 
+		
 	}
+
+	free(line);
 }
 
-void batch_mode(){
+void batch_mode(char* file_name){
 	_Bool running =1;
 	FILE *input_steam = NULL;
 
@@ -60,6 +63,37 @@ void batch_mode(){
 
 	}
 
+}
+
+void handle_line_input(char* input){
+	// remove whitespace and newline
+	clean_user_input(input);
+		
+}
+
+// Clean user input by removing newline and white spaces
+void clean_user_input(char* input){
+	// remove new line
+	input[strcspn(input, "\n")] = 0;
+	//printf("Line before processing: [%s]\n", input);
+
+	// Remove leading whitespace 
+	size_t begin_iterator = 0, end_iterator = strlen(input) - 1;
+	while (input[begin_iterator] == ' ' || input[begin_iterator] == '\t'){
+		begin_iterator ++;
+	}
+
+	// Remove trailing whitespace 
+	while (end_iterator > begin_iterator && (input[end_iterator] == ' ') || (input[end_iterator] == '\t')){
+		end_iterator --;
+	}
+
+	size_t trimmed_length = end_iterator - begin_iterator + 1;
+	memmove(input, input + begin_iterator, trimmed_length);
+	
+	input[trimmed_length] = '\0'; // update null terminating character to new sting length
+
+	//printf("Line after processing: [%s]\n", input);
 }
 
 void print_error(){
